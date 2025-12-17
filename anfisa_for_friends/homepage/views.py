0b4d50434b0ae8261e6d29aa59a0c0ee267ae764
Victoria Godovalova <vika.tom1lova@yandex.ru>
2025-12-17 17:03:27 +0500

    before new templates

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,26 @@
+from django.db.models import Q
+
 from django.shortcuts import render
+
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+
+    ice_cream_list = (
+        IceCream.objects.values(
+            'id',
+            'title',
+            'description',
+            'wrapper__title'
+        )
+        .filter(is_on_main=True, is_published=True)
+        .order_by('title')
+    )
+
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+
+    return render(request, template_name, context)
